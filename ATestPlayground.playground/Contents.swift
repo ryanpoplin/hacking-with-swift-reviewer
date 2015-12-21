32707e1a6921341843e9e8318b8a5dbec06dc50b
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2015-12-20 19:41:47 -0500

    asdfjkl;

--- ATestPlayground.playground/Contents.swift
+++ ATestPlayground.playground/Contents.swift
@@ -1,27 +1,3 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-var name = "Byrdann Fox"
-name = "Ryan Poplin"
-
-// build error, of course
-//let constantName = "Byrdann Fox"
-//constantName = "Ryan Poplin"
-
-// names are reserved for a one time usage, unless it's defined in another scope/context
-// NOTE: - maybe a better time to introduce this concept
-
-// need type annotation
-var text: String
-text = "text"
-
-var age: Int
-age = 25
-
-var latitude: Double
-latitude = 36.166667
-
-var longitude: Float
-longitude = -86.783333
-
